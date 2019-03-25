5ff7b9373c4c2b60fed9eccf900f44f45806578e
Dustin Herboldshimer and Zsuzsanna Mangu <zsuzsanna@>
2019-03-25 11:58:38 -0700

    add bmi and temperature functions

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,31 @@ var divide = function(number1, number2) {
   return number1 / number2;
 }
 
-var number1 = parseInt(prompt("Enter a number:"));
+// var number1 = parseInt(prompt("Enter a number:"));
+//
+// var number2 = parseInt(prompt("Enter another number:"));
 
-var number2 = parseInt(prompt("Enter another number:"));
+// alert(add(number1, number2));
+//
+// alert(subtract(number1, number2));
+//
+// alert(multiply(number1, number2));
+//
+// alert(divide(number1, number2));
+//
+// var height = parseFloat(prompt("what is your height in inches?"));
+// var weight = parseFloat(prompt("what is your weight in pounds?"));
+//
+// var bmi = function(height, weight) {
+//   return ((weight*.45)/((height*.025)*(height*.025))).toFixed();
+// }
+//
+// alert(bmi(height,weight));
 
-alert(add(number1, number2));
+var celcius = parseFloat(prompt("What is the temperature in Celcius?"));
 
-alert(subtract(number1, number2));
+var fahrenheit = function(celcius) {
+  return (celcius*1.8)+32;
+}
 
-alert(multiply(number1, number2));
-
-alert(divide(number1, number2));
+alert(fahrenheit(celcius));
