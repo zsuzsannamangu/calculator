a5b60aa8cd96799f4ba75359f3864927c52bed85
heather zurek and zsuzsanna mangu <heather@>
2019-03-27 11:33:36 -0700

    add javascript front end logic to calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business (or back end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 }
@@ -14,12 +16,18 @@ var divide = function(number1, number2) {
   return number1 / number2;
 }
 
-// var number1 = parseInt(prompt("Enter a number:"));
-//
-// var number2 = parseInt(prompt("Enter another number:"));
+// User interface (or front-end) logic:
 
-// alert(add(number1, number2));
-//
+$(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
+
 // alert(subtract(number1, number2));
 //
 // alert(multiply(number1, number2));
@@ -35,10 +43,10 @@ var divide = function(number1, number2) {
 //
 // alert(bmi(height,weight));
 
-var celcius = parseFloat(prompt("What is the temperature in Celcius?"));
-
-var fahrenheit = function(celcius) {
-  return (celcius*1.8)+32;
-}
-
-alert(fahrenheit(celcius));
+// var celcius = parseFloat(prompt("What is the temperature in Celcius?"));
+//
+// var fahrenheit = function(celcius) {
+//   return (celcius*1.8)+32;
+// }
+//
+// alert(fahrenheit(celcius));
